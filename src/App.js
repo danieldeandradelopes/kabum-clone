7b6a064815d74cc488520c6598113b7958c74b46
Daniel <danieldeandradelopes@gmail.com>
2020-04-21 21:31:15 -0300

    COMMITS - Added redux in project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import Routes from './routes'
 import GlobalStyle from './styles/global'
 import Header from './components/Header'
+import store from './store/index'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <GlobalStyle />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <GlobalStyle />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
