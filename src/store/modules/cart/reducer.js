a0d534edc2a91555142ef2cc4b1110a271a7860f
Daniel <danieldeandradelopes@gmail.com>
2020-04-22 00:39:47 -0300

    COMMITS - Added Redux Saga in project (have a many modifications)

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -5,7 +5,7 @@ import * as CardActions from './actions'
 export default function cart(state = [], action) {
 
     switch (action.type) {
-        case '@cart/ADD':
+        case '@cart/ADD_SUCCESS':
             return produce(state, draft => {
                 const productIndex = draft.findIndex(p => p.id === action.product.id)
 
